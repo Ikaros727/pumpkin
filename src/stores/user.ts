bcd808af6702714a9040efcc7e9bba1d7c2bde2d
Ikaros727 <ffchenyingpeng@chinatelecom.cn>
2025-03-02 21:01:34 +0800

    feat/登录注册页面样式

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -2,14 +2,7 @@ import {defineStore} from "pinia";
 import {ref} from "vue";
 import {User} from "@/dao/model/User.ts";
 import {UserModel} from "@/dao/User.ts";
-
-// 更安全的浏览器端哈希（仍需配合后端）
-async function sha256Hash(text: string) {
-    const encoder = new TextEncoder();
-    const data = encoder.encode(text);
-    const hashBuffer = await crypto.subtle.digest("SHA-256", data);
-    return Array.from(new Uint8Array(hashBuffer)).map(b => b.toString(16).padStart(2, "0")).join("");
-}
+import sha256 from "crypto-js/sha256";
 
 export const useUserStore = (deps: { userModel: UserModel }) => {
     return defineStore("user", () => {
@@ -21,34 +14,28 @@ export const useUserStore = (deps: { userModel: UserModel }) => {
 
         const register = (u: User): Promise<User> => {
             return new Promise((resolve, reject) => {
-                sha256Hash(u.Password).then((hash) => {
-                    u.Password = hash;
-                    userModel.Create(u).then(res => {
-                        resolve(res);
-                    }).catch(e => {
-                        reject(e);
-                    });
+                u.Password = sha256(u.Password).toString();
+                userModel.Create(u).then(res => {
+                    resolve(res);
                 }).catch(e => {
                     reject(e);
-                })
+                });
             });
         };
 
         const login = (username: string, password: string): Promise<User> => {
             return new Promise((resolve, reject) => {
-                sha256Hash(password).then((hash) => {
-                    password = hash;
-                    userModel.Get({
-                        Username: username, Password: password
-                    }).then(res => {
-                        if (res === null) {
-                            reject(new Error("账号或密码错误"));
-                            return
-                        }
-                        user.value = res;
-                        isAuthenticated.value = true;
-                        resolve(user.value);
-                    }).catch(reject);
+                password = sha256(password).toString();
+                userModel.Get({
+                    Username: username, Password: password
+                }).then(res => {
+                    if (res === null) {
+                        reject(new Error("账号或密码错误"));
+                        return;
+                    }
+                    user.value = res;
+                    isAuthenticated.value = true;
+                    resolve(user.value);
                 }).catch(reject);
             });
         };

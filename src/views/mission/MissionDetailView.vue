6037b4b427c955da928c073606615aec8c4d331e
Ikaros727 <ffchenyingpeng@chinatelecom.cn>
2025-03-19 00:21:14 +0800

    feat/任务详情页面

--- src/views/mission/MissionDetailView.vue
+++ src/views/mission/MissionDetailView.vue
@@ -30,11 +30,14 @@ import {MissionCarData} from "@/dao/model/MissionCarData.js";
 import {MissionCarDataModel} from "@/dao/MissionCarData.js";
 import VanCellExpand from "@/components/VanCellExpand.vue";
 import ViewTpl from "@/components/ViewTpl.vue";
+import * as paillier from "paillier-bigint";
+import {showToast} from "vant";
 
 const router = useRouter();
 const mission = ref<Mission>(new Mission());
 const missionCarDataList = ref<MissionCarData[]>([]);
 const groupedDataMap = ref<Map<string, MissionCarData[]>>(new Map<string, MissionCarData[]>());
+let paillierKey: paillier.KeyPair | null = null;
 
 let missionCarDataModel: MissionCarDataModel = null;
 
@@ -43,24 +46,80 @@ onMounted(() => {
   mission.value = new Mission(JSON.parse(route.query.mission));
   missionCarDataModel = inject("MissionCarDataModel");
   missionCarDataList.value.push(new MissionCarData({
-    Id: 1,
-    MissionId: mission.value.Id,
-    CarId: 1,
-    CarName: "比亚迪·秦",
-    Area: "福建省/福州市/鼓楼区",
+    Area: "北京市/北京市/东城区",
     DataItemList: [
-      {Type: "温度", Value: 8n},
-      {Type: "湿度", Value: 60n},
-      {Type: "降雨量", Value: 0n},
-      {Type: "噪音程度", Value: 30n}
+      {Type: "温度℃", Value: 8n},
+      {Type: "湿度%", Value: 60n},
+      {Type: "雨量mm", Value: 0n},
+      {Type: "噪音db", Value: 30n}
     ]
   }));
+  missionCarDataList.value.push(new MissionCarData({
+    Area: "北京市/北京市/东城区",
+    DataItemList: [
+      {Type: "温度℃", Value: 8n},
+      {Type: "湿度%", Value: 60n},
+      {Type: "雨量mm", Value: 0n},
+      {Type: "噪音db", Value: 30n}
+    ]
+  }));
+  missionCarDataList.value.push(new MissionCarData({
+    Area: "北京市/北京市/西城区",
+    DataItemList: [
+      {Type: "温度℃", Value: 8n},
+      {Type: "湿度%", Value: 60n},
+      {Type: "雨量mm", Value: 0n},
+      {Type: "噪音db", Value: 30n}
+    ]
+  }));
+  missionCarDataList.value.push(new MissionCarData({
+    Area: "北京市/北京市/西城区",
+    DataItemList: [
+      {Type: "温度℃", Value: 8n},
+      {Type: "湿度%", Value: 60n},
+      {Type: "雨量mm", Value: 0n},
+      {Type: "噪音db", Value: 30n}
+    ]
+  }));
+  missionCarDataList.value.push(new MissionCarData({
+    Area: "北京市/北京市/西城区",
+    DataItemList: [
+      {Type: "温度℃", Value: 8n},
+      {Type: "湿度%", Value: 60n},
+      {Type: "雨量mm", Value: 0n},
+      {Type: "噪音db", Value: 30n}
+    ]
+  }));
+  missionCarDataList.value.push(new MissionCarData({
+    Area: "北京市/北京市/西城区",
+    DataItemList: [
+      {Type: "温度℃", Value: 8n},
+      {Type: "湿度%", Value: 60n},
+      {Type: "雨量mm", Value: 0n},
+      {Type: "噪音db", Value: 30n}
+    ]
+  }));
+
   groupedDataMap.value = missionCarDataList.value.reduce((acc, curr) => {
+    console.log(acc);
     const area = curr.Area;
     if (!acc[area]) acc[area] = [];
     acc[area].push(curr);
+    console.log("[missionCarDataList.value.reduce] area:", area, "acc[area]:", acc[area]);
     return acc;
   }, {} as Record<string, MissionCarData[]>);
+  console.log(groupedDataMap.value);
+
+  paillier.generateRandomKeys(1024).then(keyPair => {
+    paillierKey = keyPair;
+    missionCarDataList.value.forEach(data => {
+      data.DataItemList.forEach(item => {
+        item.Value = paillierKey?.publicKey.encrypt(item.Value);
+      });
+    });
+  }).catch(e => {
+    showToast({message: "创建密钥失败：" + e.message});
+  });
 });
 
 const sumByDataType = (dataList: MissionCarData[], dataType: string): bigint => {
@@ -85,4 +144,48 @@ const sumByDataType = (dataList: MissionCarData[], dataType: string): bigint => 
 </script>
 
 <style scoped>
+
+:deep(.van-cell) {
+  align-items: flex-start;
+  padding: 14px 16px;
+  margin: 0 8px; /* 单元格内容左右缩进 */
+}
+
+:deep(.van-cell__title) {
+  flex: 0 0 88px;
+  min-width: 88px;
+  font-weight: 500;
+  color: #333;
+  font-size: 14px;
+}
+
+:deep(.van-cell__value) {
+  flex: 1;
+  overflow-wrap: anywhere;
+  text-align: left;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+  Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  color: #444;
+  line-height: 1.5;
+  padding-left: 12px;
+}
+
+:deep(.van-cell__value)[title="奖励金额"] {
+  color: #e55039;
+  font-weight: 500;
+}
+
+:deep(.van-cell-group__title) {
+  padding: 16px 16px 8px;
+  font-size: 16px;
+  font-weight: 600;
+  color: #2d3436;
+}
+
+/* 新增响应式边距 */
+@media (max-width: 480px) {
+  .van-cell-group {
+    margin: 12px 12px; /* 小屏幕缩小边距 */
+  }
+}
 </style>
